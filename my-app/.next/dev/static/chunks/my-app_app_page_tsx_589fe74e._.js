eeb84a4680f10b452535926db3abe7d08bf20519
Mustafa Amir Kamak <amir.kamal.28.2011@gmail.com>
2026-03-23 02:10:01 -0700

    deleated the old files caussing troble

--- my-app/.next/dev/static/chunks/my-app_app_page_tsx_589fe74e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/my-app_app_2e94162c._.js",
-  "static/chunks/07e91_a84c2094._.js"
-],
-    source: "dynamic"
-});
